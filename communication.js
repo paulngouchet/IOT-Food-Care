27a461ca3eed928b32d9a524f0341545ad44b535
paulngouchet <ngouchet@bu.edu>
2020-08-02 19:10:03 -0400

    Clean Code

--- communication.js
+++ communication.js
@@ -7,54 +7,41 @@ var text0 ;
 var text1;
 var text2;
 
-io.on('connection', function(socket)
-      {
+io.on('connection', function(socket) {
   		console.log('a user connected');
   		socket.on('disconnect', function() {});
-
 		//sp.write(Buffer([0x02]))
-  		socket.on('chat message', function(msg){
+  		socket.on('chat message', function(msg) {
 		setTimeout(read_hum, 100)
 		setTimeout(read_temp, 100)
 		setTimeout(read_light, 100)
-		setTimeout(function () 
-			   {
-				var to_send = [text0, text1, text2];
-				io.emit('chat message', { data: to_send});
-				//console.log(to_send)
-				}, 1500)
+		setTimeout(function () {
+			var to_send = [text0, text1, text2];
+			io.emit('chat message', { data: to_send});
+			//console.log(to_send)
+			}, 1500)
   		});
-	});
+});
 
-function read_hum()
-	{
-		request.get("https://us.wio.seeed.io/v1/node/GroveTempHumD0/humidity?access_token=**************",function(err,res,body){
-	 	var value = JSON.parse(body).humidity
+function read_hum(){
+	request.get("https://us.wio.seeed.io/v1/node/GroveTempHumD0/humidity?access_token=**************",function(err,res,body) {
+		var value = JSON.parse(body).humidity
 	 	console.log(value);
-		 //text0 = "The humidity level is " + value.toString() ;
-		 text0 = value
+		text0 = value
 		return text0 ;
-  		//return body["humidity"];
 	});
-
 }
 
-function read_temp()
-	{
-		request.get("https://us.wio.seeed.io/v1/node/GroveTempHumD0/temperature_f?access_token=**********",function(err,res,body){
- 		var value = JSON.parse(body).fahrenheit_degree
+function read_temp() {
+	request.get("https://us.wio.seeed.io/v1/node/GroveTempHumD0/temperature_f?access_token=**********",function(err,res,body) {
+		var value = JSON.parse(body).fahrenheit_degree
   		console.log(value);
-		//text1 = "The temperature level is " + value.toString() ;
 		text1 = value;
 		return text1 ;
-
-   		 //return body["fahrenheit_degree"];
 	});
-
 }
 
-function read_light()
-	{
+function read_light() {
 		request.get("https://us.wio.seeed.io/v1/node/GroveLuminanceA0/luminance?access_token=********",function(err,res,body){
 		var value = JSON.parse(body).lux
   		console.log(value)
@@ -69,10 +56,11 @@ function read_light()
 
 app.use(express.static('src'));
 
-app.get('/', function(req, res)
-	{
-    		res.sendFile('index.html' , { root : __dirname});
+app.get('/', function(req, res) {
+	res.sendFile('index.html' , { 
+		root : __dirname
 	});
+});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
